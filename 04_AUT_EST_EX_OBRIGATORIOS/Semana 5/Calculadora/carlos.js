3ed48766ffc383564320ea5b092290e37c445eb3
Gabi-Barretto <gabriela.dias@sou.inteli.edu.br>
2022-05-19 10:21:28 -0300

    exe - obg - semana 5

---
+++ 04_AUT_EST_EX_OBRIGATORIOS/Semana 5/Calculadora/carlos.js
@@ -0,0 +1,23 @@
+
+function CalcAndShow(){
+    
+    let valor1 = parseInt(document.getElementById("number1").value);
+    
+    let valor2 = parseInt(document.getElementById("number2").value);
+    
+    let operator = document.getElementById("op").value;
+    
+    var result = Calcu(valor1, valor2, operator);
+
+    document.getElementById("resultado").innerHTML = result;
+
+
+}
+
+function Calcu(x, y, op){
+    let result;
+    result = eval(x + op + y) 
+    return result;
+};
+   
+    
